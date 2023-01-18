b8f366944025ab1847fdf6708986cef01040b0c9
Alessandro Saggese <asaggese@gmail.com>
2023-01-18 22:35:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ let game = document.getElementById("game");
 let paddleLeft = document.getElementById("paddle-left");
 let paddleRight = document.getElementById("paddle-right");
 let ball = document.getElementById("ball");
+let player1ScoreDisplay = document.getElementById("player1-score");
+let player2ScoreDisplay = document.getElementById("player2-score");
 
 let ballX = game.clientWidth / 2 - ball.clientWidth / 2;
 let ballY = game.clientHeight / 2 - ball.clientHeight / 2;
 let ballSpeedX = 4;
 let ballSpeedY = 4;
+let player1Score = 0;
+let player2Score = 0;
 
 document.addEventListener("keydown", event => {
     if (event.code === "KeyW") {
@@ -54,3 +58,28 @@ function animate() {
 }
 
 animate();
+
+// function scorePoint(player) {
+//     if (player === 1) {
+//         player1Score++;
+//     } else {
+//         player2Score++;
+//     }
+//     updateScore();
+// }
+
+// function checkForScore() {
+//     if (ball.x <= 0) {
+//         player2Score++;
+//         updateScore();
+//     }
+//     if (ball.x >= gameWidth) {
+//         player1Score++;
+//         updateScore();
+//     }
+// }
+
+// function updateScore() {
+//     player1ScoreDisplay.innerHTML = player1Score;
+//     player2ScoreDisplay.innerHTML = player2Score;
+// }
