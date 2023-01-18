d8012a193625d4302f86bfa3efaf403bd7f669fb
Alessandro Saggese <asaggese@gmail.com>
2023-01-18 18:52:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,30 @@ let ball = document.getElementById("ball");
 
 let ballX = game.clientWidth / 2 - ball.clientWidth / 2;
 let ballY = game.clientHeight / 2 - ball.clientHeight / 2;
-let ballSpeedX = 5;
-let ballSpeedY = 5;
+let ballSpeedX = 4;
+let ballSpeedY = 4;
+
+document.addEventListener("keydown", event => {
+    if (event.code === "KeyW") {
+        paddleLeft.style.top = paddleLeft.offsetTop - 20 + "px";
+    }
+    if (event.code === "KeyS") {
+        paddleLeft.style.top = paddleLeft.offsetTop + 20 + "px";
+    }
+    if (event.code === "ArrowUp") {
+        paddleRight.style.top = paddleRight.offsetTop - 20 + "px";
+    }
+    if (event.code === "ArrowDown") {
+        paddleRight.style.top = paddleRight.offsetTop + 20 + "px";
+    }
+});
+
+game.addEventListener("mousemove", event => {
+    let gameRect = game.getBoundingClientRect();
+    let paddleLeftY = event.clientY - gameRect.top - paddleLeft.clientHeight / 2;
+    paddleLeft.style.top = paddleLeftY + "px";
+});
+
 
 function animate() {
     ballX += ballSpeedX;
