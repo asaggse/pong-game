af446e6d3bf58554dca8930f14808f3874ad3828
Alessandro Saggese <asaggese@gmail.com>
2023-01-18 22:36:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ let game = document.getElementById("game");
 let paddleLeft = document.getElementById("paddle-left");
 let paddleRight = document.getElementById("paddle-right");
 let ball = document.getElementById("ball");
-let player1ScoreDisplay = document.getElementById("player1-score");
-let player2ScoreDisplay = document.getElementById("player2-score");
+
+// let player1ScoreDisplay = document.getElementById("player1-score");
+// let player2ScoreDisplay = document.getElementById("player2-score");
 
 let ballX = game.clientWidth / 2 - ball.clientWidth / 2;
 let ballY = game.clientHeight / 2 - ball.clientHeight / 2;
 let ballSpeedX = 4;
 let ballSpeedY = 4;
-let player1Score = 0;
-let player2Score = 0;
+
+// let player1Score = 0;
+// let player2Score = 0;
 
 document.addEventListener("keydown", event => {
     if (event.code === "KeyW") {
